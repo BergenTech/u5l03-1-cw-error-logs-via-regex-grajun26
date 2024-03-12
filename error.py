1f8bb37ab643d5d12b284aa7380af21c848eaf2c
grajun26 <155980416+grajun26@users.noreply.github.com>
2024-03-12 22:52:17 +0000

    done

--- error.py
+++ error.py
@@ -1,1 +1,13 @@
+import json, csv, re
 
+with open("logs.json",'r') as jsonfile:
+    data = json.load(jsonfile)
+    with open("logs.csv",'w',newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames = data[0])
+        writer.writeheader()
+        pattern = r"^Error"
+        for x in data:
+            if re.search(pattern, x["message"]):
+                writer.writerows(data)
+            else:
+                pass
